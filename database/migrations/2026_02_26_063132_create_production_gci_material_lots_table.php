bacfcb9bfe3a1830ca6e6cfcb4d4f85656de2aef
Herlan <navylan717@gmail.com>
2026-02-26 15:24:02 +0700

    feat: refactor production_gci_work_order_id column to use explicit foreign key definition

--- database/migrations/2026_02_26_063132_create_production_gci_material_lots_table.php
+++ database/migrations/2026_02_26_063132_create_production_gci_material_lots_table.php
@@ -13,7 +13,9 @@ return new class extends Migration {
         Schema::create('production_gci_material_lots', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('production_gci_work_order_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('production_gci_work_order_id');
+            $table->foreign('production_gci_work_order_id', 'gci_material_wo_id_foreign')
+                ->references('id')->on('production_gci_work_orders')->onDelete('cascade');
             $table->string('invoice_or_tag');
             $table->integer('qty');
             $table->integer('actual');
