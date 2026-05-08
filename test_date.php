972eefd8276c73ebc0b56ed39189fa6e42ee964c
herlanyusriana <navylan717@gmail.com>
2026-05-08 14:26:45 +0700

    refactor: remove obsolete temporary test scripts and manual setup files

--- test_date.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-echo 'now(): ' . now()->toDateString() . "\n";
